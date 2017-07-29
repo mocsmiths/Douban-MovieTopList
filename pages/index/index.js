2d383160347739f08e2a0cc6bb35ef24efbd7217
mocsmiths <mocsmiths@github.com>
2017-07-29 23:14:41 +0800

    7.29
    7.29第二次更新

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,8 +1,26 @@
-var API_URL='https://api.douban.com/v2/movie/top250'
+var API_URL='https://api.douban.com/v2/movie/top250';
+
 Page({
 
   data: {
-    movies
+    movies:[]
   },
+  onLoad:function(){
+    wx.showToast({
+      title:"加载中...",
+      icon:"loading",
+      duration:10000
+    });
+    wx.request({
+      url:API_URL,
+      data:{},
+      header:{
+        'Content-Type':'application/json'
+      },
+      success:function(res){
+        console.log(res.data);
+      }
+    });
+  }
 
 })
